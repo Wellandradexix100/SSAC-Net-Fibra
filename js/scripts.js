231a5001c947ea97e7502534893d56d4171235ef
wellandrade <leto.l@hotmail.com>
2024-07-29 18:46:06 -0300

    versão final

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const nav = document.querySelector('.navigation ul');
+    nav.classList.toggle('active');
+}
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.min.css';
